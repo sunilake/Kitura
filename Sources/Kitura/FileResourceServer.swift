c6df757d2b3402ca67dc6bf21c20ffca8ee9a6d3
David Dunn <davdunn2@uk.ibm.com>
2017-12-04 17:25:53 +0000

    Remove iOS compile flags from Bundle code

--- Sources/Kitura/FileResourceServer.swift
+++ Sources/Kitura/FileResourceServer.swift
@@ -69,14 +69,10 @@ class FileResourceServer {
     private func getResourcePathBasedOnCurrentDirectory(for resource: String, withFileManager fileManager: FileManager) -> String? {
         for suffix in ["/Packages", "/.build/checkouts"] {
             let packagePath: String
-            #if os(iOS)
                 guard let resourcePath = Bundle.main.resourcePath else {
                     continue
                 }
                 packagePath = resourcePath + suffix
-            #else
-                packagePath = fileManager.currentDirectoryPath + suffix
-            #endif
 
             do {
                 let packages = try fileManager.contentsOfDirectory(atPath: packagePath)
